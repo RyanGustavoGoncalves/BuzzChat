04405f55ad11adc133919406edc2bed01835ac04
leticiaemily <leticia.godoi70@email.com>
2024-01-12 00:58:40 -0300

    structuring the code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import Home from './components/pages/Home';
-import Account from './components/pages/Account';
-import Register from './components/pages/Register';
-import Signin from './components/pages/Signin';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/pages/home';
+import Account from './components/pages/account';
+import Register from './components/pages/register';
+import Signin from './components/pages/signin';
+
+import Container from './components/layout/container'
 
 function App() {
   return (
@@ -15,23 +17,14 @@ function App() {
         <Link to='/Signin'>Sign In</Link>
       </div>
 
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-
-        <Route path='/Account'>
-          <Account />
-        </Route>
-
-        <Route path='/Register'>
-          <Register />
-        </Route>
-
-        <Route path='/Signin'>
-          <Signin />
-        </Route>
-      </Switch>
+      <Container>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Account' element={<Account />} />
+          <Route path='/Register' element={<Register />} />
+          <Route path='/Signin' element={<Signin />} />
+        </Routes>
+      </Container>
     </Router>
   );
 }
